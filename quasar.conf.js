80b18899036631181cb731fd2c622a3c00ef54af
Osama Esmail <osamaesmail2@gmail.com>
2021-11-07 10:50:19 +0200

    remove commmented code

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,11 +47,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       env: require('dotenv').config().parsed,
-      // env: {
-      //   API: ctx.dev
-      //     ? 'https://dev.api.com'
-      //     : 'https://prod.api.com'
-      // },
+
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
